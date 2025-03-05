28f862e505ec20a515d413d754978261a4496916
Tamy <tamy13vasconez@gmail.com>
2025-03-05 17:42:31 -0500

    1

--- app.py
+++ app.py
@@ -56,5 +56,6 @@ def notificacion():
     return jsonify({'mensaje': f'Tienes {len(pendientes)} facturas pendientes'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=10000)
+
 
